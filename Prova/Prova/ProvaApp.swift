fb5b94a7cb87042abb9ae7289f02d9d39a5a99fd
Kayraakbas <kayraakbas7@gmail.com>
2025-08-22 13:12:25 +0300

    Initial commit

---
+++ Prova/Prova/ProvaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProvaApp.swift
+//  Prova
+//
+//  Created by Kayra Akbaş on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ProvaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
