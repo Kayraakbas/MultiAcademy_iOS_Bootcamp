180ba16317ec0a7b374e84d840f108ad9682b4bf
Kayraakbas <kayraakbas7@gmail.com>
2025-09-05 15:39:43 +0300

    Ödev#4

---
+++ MultiAcademy-iOS-Bootcamp-4/MultiAcademy-iOS-Bootcamp-4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiAcademy-iOS-Bootcamp-4
+//
+//  Created by Kayra Akbaş on 5.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiAcademy-iOS-Bootcamp-4/MultiAcademy-iOS-Bootcamp-4/MultiAcademy_iOS_Bootcamp_4App.swift
@@ -0,0 +1,17 @@
+//
+//  MultiAcademy_iOS_Bootcamp_4App.swift
+//  MultiAcademy-iOS-Bootcamp-4
+//
+//  Created by Kayra Akbaş on 5.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiAcademy_iOS_Bootcamp_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
