1e33064615d7a3bb43bc72482d9af4635f16794d
Kayraakbas <kayraakbas7@gmail.com>
2025-09-05 15:41:22 +0300

    Ödev#5

---
+++ MultiAcademy-iOS-Bootcamp-5/MultiAcademy-iOS-Bootcamp-5/MultiAcademy_iOS_Bootcamp_5App.swift
@@ -0,0 +1,17 @@
+//
+//  MultiAcademy_iOS_Bootcamp_5App.swift
+//  MultiAcademy-iOS-Bootcamp-5
+//
+//  Created by Kayra Akbaş on 5.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiAcademy_iOS_Bootcamp_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
